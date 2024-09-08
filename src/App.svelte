906e262af08584d62dd46dd2785c43a886e6d2f6
Mel Massadian <mel@melmassadian.com>
2024-09-08 18:58:37 +0200

    feat ⚡: post reproduction

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import Button from "./Button.svelte";
+</script>
+
+<main>
+  <div class="content">
+    <h1>Rsbuild with Svelte</h1>
+    <p>Start building amazing things with Rsbuild.</p>
+    <Button />
+  </div>
+</main>
+
+<style>
+  .content {
+    display: flex;
+    min-height: 100vh;
+    line-height: 1.1;
+    text-align: center;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .content h1 {
+    font-size: 3.6rem;
+    font-weight: 700;
+  }
+
+  .content p {
+    font-size: 1.2rem;
+    font-weight: 400;
+    opacity: 0.5;
+  }
+</style>
---
+++ src/Button.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import { Button } from "bits-ui";
+</script>
+ 
+<Button.Root />
